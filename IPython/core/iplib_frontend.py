704d3f01994234d90af6d865c861046bd3f85285
Omar Andrés Zapata Mesa <andresete.chaos@gmail.com>
2010-06-06 23:44:43 -0500

    ipython prompt is ready, suppor indentation and it dont run code in this moment

--- IPython/core/iplib_frontend.py
+++ IPython/core/iplib_frontend.py
@@ -4,6 +4,8 @@
 
 import __builtin__
 from contextlib import nested
+import time
+import readline
 
 from IPython.core.iplib import InteractiveShell
 from IPython.utils import session
@@ -14,18 +16,57 @@ from IPython.utils.traitlets import (
 )
 
 class InteractiveShellFrontend(InteractiveShell):
-   def __init__(self,locals=None, filename="<console>", session = session, request_socket=None, subscribe_socket=None):
+   def __init__(self,filename="<console>", session = session, request_socket=None, subscribe_socket=None):
        InteractiveShell.__init__(self)
-       self.shell=InteractiveShell()
-       self.display_banner = CBool(False)
-       #self._completer=completer.ClientCompleter(None,session,request_socket)
-       #self.set_custom_completer(self._completer)
+       self.buffer_lines=[]
+       #self.shell=InteractiveShell()
+       #self.display_banner = CBool(False)
        
+       #self.completer=completer.ClientCompleter(self,session,request_socket)
+       #self.set_custom_completer(self.completer)
+       #self.set_completer()
        
-   def request(self,message):
-       pass
+       #readline.parse_and_bind('tab: complete')
+       #readline.parse_and_bind('set show-all-if-ambiguous on')
+       #readline.set_completer(self.completer.complete)
+       #self.set_custom_completer(self.completer.complete,0)
+#       self.handlers = {}
+#       for msg_type in ['pyin', 'pyout', 'pyerr', 'stream']:
+#           self.handlers[msg_type] = getattr(self, 'handle_%s' % msg_type)
+#       self.session = session
+#       self.request_socket = request_socket
+#       self.sub_socket = subscribe_socket
+#       self.backgrounded = 0
+#       self.messages = {}
+   
+   def test_push_line(self,line):
+       for subline in line.splitlines():
+            self._autoindent_update(subline)
+       self.buffer_lines.append(line)
+       more = self.test_runsource('\n'.join(self.buffer_lines), self.filename)
+       if not more:
+           self.resetbuffer()
+       return more
+   
+   def test_runsource(self, source, filename='<input>', symbol='single'):
+       source=source.encode(self.stdin_encoding)
+       if source[:1] in [' ', '\t']:
+           source = 'if 1:\n%s' % source
+       try:
+           code = self.compile(source,filename,symbol)
+       except (OverflowError, SyntaxError, ValueError, TypeError, MemoryError):
+            # Case 1
+           self.showsyntaxerror(filename)
+           return None
+
+       if code is None:
+            # Case 2
+           return True
+       else:
+           self.buffer_lines[:]=[]
+           return False
     
-   def run(self):
+   def prompt(self):
         """Closely emulate the interactive Python console."""
 
         # batch run -> do not interact        
@@ -38,7 +79,8 @@ class InteractiveShellFrontend(InteractiveShell):
         #    self.show_banner()
 
         more = 0
-        
+        #__builtin__.__dict__['__IPYTHON__active'] += 1
+         
         if self.has_readline:
             self.readline_startup_hook(self.pre_readline)
         # exit_now is set by a call to %Exit or %Quit, through the
@@ -98,16 +140,141 @@ class InteractiveShellFrontend(InteractiveShell):
                 # exceptions here are VERY RARE, but they can be triggered
                 # asynchronously by signal handlers, for example.
                 self.showtraceback()
-            print buffer
+            else:
+                more = self.test_push_line(line)
+                if (self.SyntaxTB.last_syntax_error and
+                    self.autoedit_syntax):
+                    self.edit_syntax_error()
+            #print buffer    
+            #self.runcode(buffer)
               
         # We are off again...
         #__builtin__.__dict__['__IPYTHON__active'] -= 1
 
         # Turn off the exit flag, so the mainloop can be restarted if desired
         self.exit_now = False
-
-    
+        
+#   def handle_pyin(self, omsg):
+#       if omsg.parent_header.session == self.session.session:
+#            return
+#       c = omsg.content.code.rstrip()
+#       if c:
+#           print '[IN from %s]' % omsg.parent_header.username
+#           print c
+#   def handle_pyout(self, omsg):
+#       #print omsg # dbg
+#       if omsg.parent_header.session == self.session.session:
+#           print "%s%s" % (sys.ps3, omsg.content.data)
+#       else:
+#           print '[Out from %s]' % omsg.parent_header.username
+#           print omsg.content.data
+#   def print_pyerr(self, err):
+#       print >> sys.stderr, err.etype,':', err.evalue
+#       print >> sys.stderr, ''.join(err.traceback)       
+#    
+#   def handle_pyerr(self, omsg):
+#       if omsg.parent_header.session == self.session.session:
+#           return
+#       print >> sys.stderr, '[ERR from %s]' % omsg.parent_header.username
+#       self.print_pyerr(omsg.content)
+#       
+#   def handle_stream(self, omsg):
+#       if omsg.content.name == 'stdout':
+#           outstream = sys.stdout
+#       else:
+#           outstream = sys.stderr
+#           print >> outstream, '*ERR*',
+#           print >> outstream, omsg.content.data,
+#
+#   def handle_output(self, omsg):
+#        handler = self.handlers.get(omsg.msg_type, None)
+#        if handler is not None:
+#            handler(omsg)
+#
+#   def recv_output(self):
+#        while True:
+#            omsg = self.session.recv(self.sub_socket)
+#            if omsg is None:
+#                break
+#            self.handle_output(omsg)
+#
+#   def handle_reply(self, rep):
+#        # Handle any side effects on output channels
+#        self.recv_output()
+#        # Now, dispatch on the possible reply types we must handle
+#        if rep is None:
+#            return
+#        if rep.content.status == 'error':
+#            self.print_pyerr(rep.content)            
+#        elif rep.content.status == 'aborted':
+#            print >> sys.stderr, "ERROR: ABORTED"
+#            ab = self.messages[rep.parent_header.msg_id].content
+#            if 'code' in ab:
+#                print >> sys.stderr, ab.code
+#            else:
+#                print >> sys.stderr, ab
+#
+#   def recv_reply(self):
+#        rep = self.session.recv(self.request_socket)
+#        self.handle_reply(rep)
+#        return rep
+#
+#   def runcode(self, code):
+#       # We can't pickle code objects, so fetch the actual source
+#       src = '\n'.join(self.buffer)
+#       # for non-background inputs, if we do have previoiusly backgrounded
+#       # jobs, check to see if they've produced results
+#       if not src.endswith(';'):
+#           while self.backgrounded > 0:
+#               #print 'checking background'
+#               rep = self.recv_reply()
+#               if rep:
+#                   self.backgrounded -= 1
+#               time.sleep(0.05)
+#       # Send code execution message to kernel
+#       omsg = self.session.send(self.request_socket,
+#                                 'execute_request', dict(code=src))
+#       self.messages[omsg.header.msg_id] = omsg
+#        
+#        # Fake asynchronicity by letting the user put ';' at the end of the line
+#       if src.endswith(';'):
+#           self.backgrounded += 1
+#           return
+#
+#        # For foreground jobs, wait for reply
+#       while True:
+#           rep = self.recv_reply()
+#           if rep is not None:
+#               break
+#           self.recv_output()
+#           time.sleep(0.05)
+#       else:
+#           # We exited without hearing back from the kernel!
+#           print >> sys.stderr, 'ERROR!!! kernel never got back to us!!!'
+#    
       
 if __name__ == "__main__" :
-    frontend=InteractiveShellFrontend()
-    frontend.run()
+    # Defaults
+    #ip = '192.168.2.109'
+    import zmq
+    ip = '127.0.0.1'
+    #ip = '99.146.222.252'
+    port_base = 5555
+    connection = ('tcp://%s' % ip) + ':%i'
+    req_conn = connection % port_base
+    sub_conn = connection % (port_base+1)
+    
+    # Create initial sockets
+    c = zmq.Context()
+    request_socket = c.socket(zmq.XREQ)
+    request_socket.connect(req_conn)
+    
+    sub_socket = c.socket(zmq.SUB)
+    sub_socket.connect(sub_conn)
+    sub_socket.setsockopt(zmq.SUBSCRIBE, '')
+
+    # Make session and user-facing client
+    sess = session.Session()
+    
+    frontend=InteractiveShellFrontend('<zmq-console>',sess,request_socket=request_socket,subscribe_socket=sub_socket)
+    frontend.prompt()
